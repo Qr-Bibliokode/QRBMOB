d4a4a81643b232f5dad2ce70bce7e6beea5269d9
felansu <gaferran@gmail.com>
2015-11-27 00:27:34 -0200

    - Acrescentado tela principal onde mostrará as noticias do sistema - Criada página noticias - Criada página detalhes noticia - Criada página livros locados - Template pra tela consulta - Ocultados menus caso não estiver logado

--- www/js/livrosLocadosController.js
+++ www/js/livrosLocadosController.js
@@ -18,14 +18,12 @@
         sinopse: "Mesmo um código ruim pode funcionar. Mas se ele não for limpo, pode acabar " +
         "com uma empresa de desenvolvimento. Perdem-se a cada ano horas incontáveis e recursos " +
         "importantes devido a um código mal escrito. O especialista em software, Robert C. Martin," +
-        " apresenta um paradigma com 'Código limpo - Habilidades Práticas do Agile Software.' " +
+        " apresenta um paradigma com 'Código limpo - Habilidades Práticas do Agile Software. ' " +
         "O leitor poderá aprender a ler códigos e descobrir o que está correto e errado neles. " +
         "'Código limpo' está divido em três partes. Na primeira há diversos capítulos que descrevem " +
         "os princípios, padrões e práticas para criar um código limpo. A segunda parte consiste em " +
         "diversos casos de estudo de complexidade cada vez maior. Cada um é um exercício para limpar " +
-        "um código - transformar o código base que possui alguns problemas em melhores e mais eficientes. " +
-        "A terceira parte é a compensação - um único capítulo com uma lista de heurísticas e 'odores' " +
-        "reunidos durante a criação dos estudos de caso."
+        "um código - transformar o código base que possui alguns problemas em melhores e mais eficientes. "
       }
     ];
 
